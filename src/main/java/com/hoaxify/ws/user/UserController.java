ff4f60aaa0d4b5972d05deba90245204b1ed6d56
Adınız <erkanmurat0632@gmail.com>
2024-04-17 15:12:48 +0300

    proxy config added

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,11 +1,9 @@
 package com.hoaxify.ws.user;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin
 public class UserController {
 @PostMapping("/api/v1/users")
 void createUser (){
